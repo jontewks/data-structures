fda7eb3429946d8c281a0ff2502def10c1c65835
Hack Reactor Students <jontewks@gmail.com>
2014-02-26 12:39:02 -0800

    Almost completed functional implementation of queue with one test left failing.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
-
   instance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.dequeue = function(){
+    var item = storage[0];
+    if (item !== undefined) {
+      delete storage[0];
+      size--;
+      for (var key in storage) {
+        key--;
+      }
+      return item;
+    }
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
