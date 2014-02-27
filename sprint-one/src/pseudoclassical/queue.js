ee5f3245fb8eb6196a3df8102a3d64ab637e2208
Hack Reactor Students <jontewks@gmail.com>
2014-02-26 16:24:01 -0800

    Finished pseudoclassical queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,27 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.currentSize = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.currentSize] = value;
+  this.currentSize++;
+};
+
+Queue.prototype.dequeue = function(){
+  var item = this.storage[0];
+  if (item !== undefined) {
+    delete this.storage[0];
+    this.currentSize--;
+    for (var key in this.storage) {
+      var current = 0;
+      this.storage[current] = this.storage[key];
+    }
+    return item;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.currentSize;
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,29 @@
+// /*global _*/
+
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.currentSize = 0;
+
 };
+
+
+Stack.prototype.push = function(value){
+    this.storage[this.currentSize] = value;
+    this.currentSize++;
+  };
+
+Stack.prototype.pop = function(){
+  var item = this.storage[this.currentSize-1];
+  if (item !== undefined) {
+    delete this.storage[this.currentSize-1];
+    this.currentSize--;
+    return item;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.currentSize;
+};
+
+var newStack = new Stack();
