e08f4f5d9e352b6a9931b3a33e338643945cfe16
Josh <jsgoldberg90@gmail.com>
2014-02-27 19:11:36 -0800

    finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,21 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	this[item] = item;
+	console.log(this);
 };
 
 setPrototype.contains = function(item){
+	verdict = false;
+
+	for (var key in this) {
+		if (this[key] === item) {
+			verdict = true;
+		}
+	}
+	return verdict
 };
 
 setPrototype.remove = function(item){
+	delete this[item];
 };
