433120d6c1c60f472ce9b27e0369eab700cea414
Josh <jsgoldberg90@gmail.com>
2014-02-27 18:57:09 -0800

    finished linkedList and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,33 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var lastNode;
 
   list.addToTail = function(value){
+
+    var node = makeNode(value);
+    if (this.head === null) {
+      list.head = list.tail = node;
+      list.head.next = value;
+    } else {
+      lastNode = list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var val = list.head.value;
+    var newHead = lastNode;
+    delete lastNode;
+    list.head = newHead;
+    return val;
   };
 
   list.contains = function(target, node){
+    if (this.head.value === target || this.tail.value === target) {
+      return true;
+    } else {
+      return this.target !== undefined;
+    }
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,16 +1,39 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  extend(newTree, treeMethods);
+
   return newTree;
+};
+
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
 };
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var child = makeTree(value);
+  this.children.push(child);
+
 };
 
 treeMethods.contains = function(target){
-};
+  var verdict = {false: false};
 
+  if (this.value === target) {
+    verdict.true = true;
+  } else if (this.children.length) {
+    for (var i = 0; i < this.children.length; i++) {
+   	  var result = this.children[i].contains(target)
+      verdict[result] = result;
+    }
+  }
+  
+  return verdict.true ? verdict.true : verdict.false;
+
+};
