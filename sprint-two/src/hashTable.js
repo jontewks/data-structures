b4a043ab6f765ad50e51b2e51f6730b2ccfe5952
Hack Reactor Students <jontewks@gmail.com>
2014-03-01 12:02:04 -0800

    Complete extra credit for Hash Tables and change spec to run extra credit tests.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,11 +1,24 @@
 var HashTable = function(){
   this._limit = 8;
+  this._size = 0;
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   this._storage.set(i, v);
+  this._size++;
+  if (this._size > (this._limit * 0.75)) {
+    var newStorage = makeLimitedArray(this._limit * 2)
+    this._storage.each(function(item, key, collection){
+      if (item !== undefined) {
+        var x = getIndexBelowMaxForKey(k, this._limit/2);
+        newStorage.set(x, item);
+      }
+    });
+    this._storage = newStorage;
+    this._limit = 2 * this._limit;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -21,5 +34,18 @@ HashTable.prototype.remove = function(k){
       delete collection[i];
     }
   });
+
+  this._size--;
+  if (this._size < (this._limit * 0.25)) {
+    var newStorage = makeLimitedArray(this._limit/2);
+    this._storage.each(function(item, key, collection){
+      if (item !== undefined) {
+        var x = getIndexBelowMaxForKey(k, this._limit/2);
+        newStorage.set(x, item);
+      }
+    });
+    this._storage = newStorage;
+    this._limit = this._limit/2;
+  }
 };
 
